fff3381e93fc7dfc65eb0984c6c796580c6bee4c
Eric Faraut <eric.faraut@hotmail.fr>
2023-10-18 10:30:17 +0200

    SDK 8.0.100 RC2 update.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string expectedHash = "b28a3f288a151a6a6e76ffd28a33ab92712a8308165819d469cacb1c547168a2bf7f78b975346cdf7b3f01b2d9b50070935588adc0ae1d03cfaff0744242423e";
-string path = @"C:\softwareDownloads\dotnet-sdk-8.0.100-rc.1.23463.5-win-x64.exe";
+﻿string expectedHash = "f4c4ebdaa684b2f7cee1b0034749dcde2a6c8b9a5e45fe4b6f1ff4c918367b6e59d23ec1f70a2db3a7ec9867f85507615aa0467a35406b08ed925ec5abf4b49a";
+string path = @"C:\softwareDownloads\dotnet-sdk-8.0.100-rc.2.23502.2-win-x64.exe";
 string SHA = "SHA512";
 CheckFileHash.Helpers.HashHelpers.MatchHashes
 (
