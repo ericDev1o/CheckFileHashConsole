ceae4966309032871b3479ef869375153157a58e
Eric Faraut <eric.faraut@hotmail.fr>
2023-11-22 17:18:11 +0100

    The tool is updated to dotnet 8.0.100.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string expectedHash = "f4c4ebdaa684b2f7cee1b0034749dcde2a6c8b9a5e45fe4b6f1ff4c918367b6e59d23ec1f70a2db3a7ec9867f85507615aa0467a35406b08ed925ec5abf4b49a";
-string path = @"C:\softwareDownloads\dotnet-sdk-8.0.100-rc.2.23502.2-win-x64.exe";
+﻿string expectedHash = "248acec95b381e5302255310fb9396267fd74a4a2dc2c3a5989031969cb31f8270cbd14bda1bc0352ac90f8138bddad1a58e4af1e56cc4a1613b1cf2854b518e";
+string path = @"C:\softwareDownloads\dotnet-sdk-8.0.100-win-x64.exe";
 string SHA = "SHA512";
 CheckFileHash.Helpers.HashHelpers.MatchHashes
 (
