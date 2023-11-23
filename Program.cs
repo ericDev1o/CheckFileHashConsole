b3d92c51ee2566dbfc7e2f5a61a23526f96755df
Eric Faraut <eric.faraut@hotmail.fr>
2023-11-23 17:12:51 +0100

    linux usage is OK.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿string expectedHash = "248acec95b381e5302255310fb9396267fd74a4a2dc2c3a5989031969cb31f8270cbd14bda1bc0352ac90f8138bddad1a58e4af1e56cc4a1613b1cf2854b518e";
-string path = @"C:\softwareDownloads\dotnet-sdk-8.0.100-win-x64.exe";
-string SHA = "SHA512";
+﻿string expectedHash = "4fc5817f8bfd86bdb3af924d3ca32e349517710ac5f986ae20f765f79285e00b";
+string path = @"/home/utilisateur/Téléchargements/ideaIC-2023.2.5.tar.gz";
+string SHA = "SHA256";
 CheckFileHash.Helpers.HashHelpers.MatchHashes
 (
     expectedHash, 
