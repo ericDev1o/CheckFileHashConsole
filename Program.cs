b8d40eec3047b1b958f85a6fdceb7aeb96fa3397
Eric Faraut <eric.faraut@hotmail.fr>
2024-04-13 02:30:43 +0200

    dotnet SDK 8.0.204 update is done.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string expectedHash = "f6f5609134d4006a16b96c2ca3be32acf7492bb7db233690204141a95952077018dc5f1503fd4dedf7ec44253a3427373fa21dab778a3b3d3d3d37df90c11228";
-string path = @"C:\softwareDownloads\dotnet-sdk-8.0.203-win-x64.exe";
+﻿string expectedHash = "9c3bf9c027131a3ad9f66667c609b19e8846af795fa5a88558b49c6d6011d571aa1faea76f7b6a2dc5f5eb30d9ab5db7a4a9bea2348642b23c9968a4b73f3c11";
+string path = @"C:\softwareDownloads\dotnet-sdk-8.0.204-win-x64.exe";
 string SHA = "SHA512";
 CheckFileHash.Helpers.HashHelpers.MatchHashes
 (
