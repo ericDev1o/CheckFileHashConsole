4dee0fd4f366e1096a2f7e88d9ed88503ad3e87a
Eric Faraut <eric.faraut@hotmail.fr>
2024-09-23 09:56:35 +0200

    September dotnet 9.0.100 rc1 update is done.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string expectedHash = "53a429eec91b4335d2ff1822bd2b41c826b06a53d187d24645a991066f28498d93f06557bd1c471a9334dc02bcdb3b6d075cfa9d6c894bdfc0f29c0b6e0d7a02";
-string path = @"C:\softwareDownloads\dotnet-sdk-9.0.100-preview.7.24407.12-win-x64.exe";
+﻿string expectedHash = "f8f3002242c76b5eb786b8c7788556563a22151a7e09032e7b0edcf4de17c3c69cf7f8dd598b2c5752df62d40798bcc4bc63f6bed8ce377d0938fe8b0ce631d1";
+string path = @"C:\softwareDownloads\dotnet-sdk-9.0.100-rc.1.24452.12-win-x64.exe";
 string SHA = "SHA512";
 CheckFileHash.Helpers.HashHelpers.MatchHashes
 (
